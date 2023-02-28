39c6870686962abab34a9d73346a7df74d713a47
Anton Surkis <antonsurkis@gmail.com>
2023-02-28 15:37:25 +0300

    Add documentation

--- src/Phases/Parser.hs
+++ src/Phases/Parser.hs
@@ -12,6 +12,11 @@ import           Prelude
 error' :: String -> Error
 error' = Error "ParsingError"
 
+-- | Функция для разбора строки после подстановки переменных.
+-- Выделяет либо команду с аргументами,
+-- либо пустую команду (пустая строка или строка только из пробелов),
+-- либо присваивание переменной.
+-- Кидает исключение, если строка имеет некорректный синтаксис.
 parser :: MonadError m => String -> m Primitive
 parser s = skipSpaces s >>= firstWord
 
@@ -24,6 +29,7 @@ firstWord :: MonadError m => String -> m Primitive
 firstWord     ""                 = pure $ Command []
 firstWord   ( '\\' : '\\' : cs ) = prependChar '\\' <$> firstWord cs
 firstWord   ( '\\' : '\"' : cs ) = prependChar '\"' <$> firstWord cs
+firstWord   ( '\\' : '\'' : cs ) = prependChar '\'' <$> firstWord cs
 firstWord   ( '\\' : ' '  : cs ) = Command . filter (not . null) . headMap (' ' :) <$> splitBySpaces cs
 firstWord   ( '\\' : '='  : cs ) = Command . filter (not . null) . headMap ('=' :) <$> splitBySpaces cs
 firstWord   (        ' '  : cs ) = Command . ("" :) . filter (not . null)          <$> splitBySpaces cs
@@ -50,6 +56,7 @@ splitBySpaces  ""                = pure [""]
 splitBySpaces  "\\"              = throwError $ error' "Unexpected end of line after \\"
 splitBySpaces ('\\' : '\\' : cs) = headMap ('\\' :) <$> splitBySpaces cs
 splitBySpaces ('\\' : '\"' : cs) = headMap ('\"' :) <$> splitBySpaces cs
+splitBySpaces ('\\' : '\'' : cs) = headMap ('\'' :) <$> splitBySpaces cs
 splitBySpaces ('\\' : ' '  : cs) = headMap (' ' :) <$> splitBySpaces cs
 splitBySpaces (       '\'' : cs) = singleQuotes cs >>= \(s, r) -> headMap (s ++) <$> splitBySpaces r
 splitBySpaces (       '\"' : cs) = doubleQuotes cs >>= \(s, r) -> headMap (s ++) <$> splitBySpaces r
@@ -70,6 +77,7 @@ doubleQuotes :: MonadError m => String -> m (String, String)
 doubleQuotes "" = throwError $ error' "Unexpected end of line in double quotes"
 doubleQuotes ('\\' : '\\' : cs) = first ('\\' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : '\"' : cs) = first ('\"' :) <$> doubleQuotes cs
+doubleQuotes ('\\' : '\'' : cs) = first ('\'' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : 'n'  : cs) = first ('\n' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : 't'  : cs) = first ('\t' :) <$> doubleQuotes cs
 doubleQuotes (       '\"' : cs) = pure ("", cs)
