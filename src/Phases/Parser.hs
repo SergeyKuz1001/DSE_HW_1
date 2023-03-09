b87dbce808a50bf4b2127965e8a674d5d85d8e98
Anton Surkis <antonsurkis@gmail.com>
2023-03-09 18:33:38 +0300

    [no ci] Add pipes parsing

--- src/Phases/Parser.hs
+++ src/Phases/Parser.hs
@@ -3,100 +3,132 @@
 
 {- |
 Модуль для парсинга пользовательского запроса.
+
+Весь этот модуль выглядел бы куда красивее на парсер-комбинаторах,
+но их использовать явно запрещено в задании.
 -}
 module Phases.Parser ( parser ) where
 
-import           Data.Primitive          (Primitive (..))
-import           Environment.MonadError  (Error (..), MonadError, throwError)
-
-import           Data.Bifunctor          (first)
+import           Control.Monad
+import           Data.Bifunctor         (first)
+import           Data.Char
+import qualified Data.Primitive         as P
+import           Environment.MonadError (Error (..), MonadError, throwError)
 import           Prelude
 
 error' :: String -> Error
 error' = Error "ParsingError"
 
--- | Функция для разбора строки после подстановки переменных.
--- Выделяет либо команду с аргументами,
--- либо пустую команду (пустая строка или строка только из пробелов),
--- либо присваивание переменной.
--- Кидает исключение, если строка имеет некорректный синтаксис.
-parser :: MonadError m => String -> m Primitive
-parser = undefined
-{-parser s = skipSpaces s >>= firstWord
+data Primitive
+  = Commands [[String]]
+  | Assignment String String
+  deriving (Eq, Show)
 
--- | Шаг парсера, пропускающий пробелы и табуляции до первого значащего символа.
-skipSpaces :: Applicative f => String -> f String
-skipSpaces (' '  : cs) = skipSpaces cs
-skipSpaces ('\t' : cs) = skipSpaces cs
-skipSpaces s           = pure s
+{- |
+Функция для разбора строки после подстановки переменных.
+Выделяет либо конвейер команд,
+либо пустую команду (пустая строка или строка только из пробелов),
+либо присваивание переменной.
+Кидает исключение, если строка имеет некорректный синтаксис.
+-}
+parser :: MonadError m => String -> m P.Primitive
+parser s = firstWord (dropWhile isSpace s) >>= removeNulls
 
--- | Определение первого слова.
--- Здесь происходит выбор типа строки: пустая строка, команда, присваивание.
+{- |
+Убирает пустые строки из команд,
+затем проверяет, что все команды корректны,
+т.е. содержат хотя бы одну строку.
+-}
+removeNulls :: MonadError m => Primitive -> m P.Primitive
+removeNulls (Assignment x v) = pure $ P.Assignment x v
+removeNulls (Commands [])    = pure $ P.Commands []
+removeNulls (Commands lst)   = fmap P.Commands $ forM lst $ \args -> case filter (not . null) args of
+  []       -> throwError $ error' "Empty command between pipes"
+  (x : xs) -> pure $ x : xs
+
+{- |
+Определение первого слова.
+Здесь происходит выбор типа строки:
+пустая строка, конвейер команд, присваивание.
+-}
 firstWord :: MonadError m => String -> m Primitive
-firstWord     ""                 = pure $ Command []
-firstWord   ( '\\' : '\\' : cs ) = firstWord cs >>= prependChar '\\'
-firstWord   ( '\\' : '\"' : cs ) = firstWord cs >>= prependChar '\"'
-firstWord   ( '\\' : '\'' : cs ) = firstWord cs >>= prependChar '\''
-firstWord   ( '\\' : ' '  : cs ) = Command . filter (not . null) . headMap (' ' :) <$> splitBySpaces cs
-firstWord   ( '\\' : '='  : cs ) = Command . filter (not . null) . headMap ('=' :) <$> splitBySpaces cs
-firstWord   (        ' '  : cs ) = Command . ("" :) . filter (not . null)          <$> splitBySpaces cs
-firstWord   (        '\t' : cs ) = firstWord (' ' : cs)
-firstWord s@(        '\'' : _  ) = Command . filter (not . null) <$> splitBySpaces s
-firstWord s@(        '\"' : _  ) = Command . filter (not . null) <$> splitBySpaces s
-firstWord   (        '='  : cs ) = Assignment <$> varName "" <*> parseValue cs
-firstWord   (        c    : cs ) = firstWord cs >>= prependChar c
+firstWord "" = pure $ Commands []
+firstWord ('=' : cs) = Assignment "" <$> parseValue cs
+firstWord (c : cs) | isIdent c = prependChar c <$> firstWord cs
+firstWord (c : cs) | isSpace c = Commands . headMap ("" :) <$> parseCommands cs
+firstWord s = Commands <$> parseCommands s
 
--- | Вспомогательная функция для сохранения первого слова в команде.
--- Конечный автомат сохраняет состояния в виде цепочки
--- >>> prependChar 'c' =<< prependChar 'm' =<< pure (Command [['d']])
-prependChar :: MonadError m => Char -> Primitive -> m Primitive
-prependChar c (Assignment name value) = flip Assignment value <$> addChar c name
-prependChar c (Command [])            = return $ Command [[c]]
-prependChar c (Command (w : ws))      = return . Command $ (c : w) : ws
+{- | Определяет корректные символы для идентификаторов -}
+isIdent :: Char -> Bool
+isIdent c = isAlphaNum c || isDigit c || c == '_'
 
--- | Чтение значения переменной в присваивании.
+{- |
+Вспомогательная функция для сохранения первого слова в команде.
+Конечный автомат сохраняет состояния в виде цепочки
+>>> prependChar 'c' <$> prependChar 'm' <$> pure (Commands [["d", "arg1"]])
+-}
+prependChar :: Char -> Primitive -> Primitive
+prependChar c (Assignment name value)      = Assignment (c : name) value
+prependChar c (Commands [])                = Commands [[[c]]]
+prependChar c (Commands ([] : cmds))       = Commands $ [[c]] : cmds
+prependChar c (Commands ((w : ws) : cmds)) = Commands $ ((c : w) : ws) : cmds
+
+{- | Чтение значения переменной в присваивании. -}
 parseValue :: MonadError m => String -> m String
 parseValue s = splitBySpaces s >>= \case
-  [] -> pure ""
-  [x] -> pure x
-  (_ : _) -> throwError $ error' "Command calls with variable overriding are not supported"
-  . filter (not . null)
+  ([], []) -> pure ""
+  ([x], []) -> pure x
+  (_ : _, _) -> throwError $ error' "Command calls with variable overriding are not supported"
+  (_, _ : _) -> throwError $ error' "Unexpected pipe"
+  . first (filter (not . null))
 
--- | Разбиение произвольной строки по пробелам
--- с учётом кавычек (возможны пустые строки,
--- используется в связке с @filter (not . null)@.
-splitBySpaces :: MonadError m => String -> m [String]
-splitBySpaces  ""                = pure [""]
-splitBySpaces  "\\"              = throwError $ error' "Unexpected end of line after \\"
-splitBySpaces ('\\' : '\\' : cs) = headMap ('\\' :) <$> splitBySpaces cs
-splitBySpaces ('\\' : '\"' : cs) = headMap ('\"' :) <$> splitBySpaces cs
-splitBySpaces ('\\' : '\'' : cs) = headMap ('\'' :) <$> splitBySpaces cs
-splitBySpaces ('\\' : ' '  : cs) = headMap (' ' :) <$> splitBySpaces cs
-splitBySpaces (       '\'' : cs) = singleQuotes cs >>= \(s, r) -> headMap (s ++) <$> splitBySpaces r
-splitBySpaces (       '\"' : cs) = doubleQuotes cs >>= \(s, r) -> headMap (s ++) <$> splitBySpaces r
-splitBySpaces (       ' '  : cs) = ("" :) <$> splitBySpaces cs
-splitBySpaces (       '\t' : cs) = ("" :) <$> splitBySpaces cs
-splitBySpaces (        c   : cs) = headMap (c :) <$> splitBySpaces cs
+{- |
+Разбиение конвейера команд с учётом пайпов.
+Вызывается из firstWord либо начиная с последнего символа команды,
+либо с символа после пробельного.
+Не проверяет пустые команды, этим занимается уже removeNulls.
+-}
+parseCommands :: MonadError m => String -> m [[String]]
+parseCommands "" = pure []
+parseCommands s = do
+  (args, rest) <- splitBySpaces s
+  cmds <- parseCommands rest
+  pure $ args : cmds
 
--- | Применение функции только к голове списка,
--- если она есть.
+{- |
+Разбиение строки по пробелам
+с учётом кавычек (возможны пустые строки,
+используется в связке с @filter (not . null)@.
+Останавливается на символе конвейера @|@.
+-}
+splitBySpaces :: MonadError m => String -> m ([String], String)
+splitBySpaces (        '|'  : cs ) = pure ([""], cs)
+splitBySpaces   ""                 = pure ([""], "")
+splitBySpaces   "\\"               = throwError $ error' "Unexpected end of line after \\"
+splitBySpaces ( '\\' :  c   : cs ) = first (headMap (c :)) <$> splitBySpaces cs
+splitBySpaces (        '\'' : cs ) = singleQuotes cs >>= \(s, r) -> first (headMap (s ++)) <$> splitBySpaces r
+splitBySpaces (        '\"' : cs ) = doubleQuotes cs >>= \(s, r) -> first (headMap (s ++)) <$> splitBySpaces r
+splitBySpaces (         c   : cs ) | isSpace c = first ("" :) <$> splitBySpaces cs
+splitBySpaces (         c   : cs ) = first (headMap (c :)) <$> splitBySpaces cs
+
+{- | Применение функции только к голове списка, если она есть. -}
 headMap :: (a -> a) -> [a] -> [a]
 headMap _ []       = []
 headMap f (x : xs) = f x : xs
 
--- | Чтение фрагмента строки, заключённого в одинарные кавычки.
+{- | Чтение фрагмента строки, заключённого в одинарные кавычки. -}
 singleQuotes :: MonadError m => String -> m (String, String)
 singleQuotes "" = throwError $ error' "Unexpected end of line in single quotes"
 singleQuotes ('\'' : cs) = pure ("", cs)
 singleQuotes ( c   : cs) = first (c :) <$> singleQuotes cs
 
--- | Чтение фрагмента строки, заключённого в двойные кавычки.
+{- | Чтение фрагмента строки, заключённого в двойные кавычки. -}
 doubleQuotes :: MonadError m => String -> m (String, String)
 doubleQuotes "" = throwError $ error' "Unexpected end of line in double quotes"
+doubleQuotes (       '\"' : cs) = pure ("", cs)
 doubleQuotes ('\\' : '\\' : cs) = first ('\\' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : '\"' : cs) = first ('\"' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : '\'' : cs) = first ('\'' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : 'n'  : cs) = first ('\n' :) <$> doubleQuotes cs
 doubleQuotes ('\\' : 't'  : cs) = first ('\t' :) <$> doubleQuotes cs
-doubleQuotes (       '\"' : cs) = pure ("", cs)
-doubleQuotes (        c   : cs) = first (c :) <$> doubleQuotes cs-}
+doubleQuotes (        c   : cs) = first (c :) <$> doubleQuotes cs
--- test/Phases/Parser/Tests.hs
+++ test/Phases/Parser/Tests.hs
@@ -2,11 +2,9 @@
 
 module Phases.Parser.Tests (testsParser) where
 
-import           Data.VarName
 import           Data.Primitive
 import qualified Environment.MonadError as E
 import           Phases.Parser
-
 import           Control.Monad.Except
 import           Test.HUnit
 
@@ -16,37 +14,38 @@ newtype TestEnvironment a = TestEnvironment (Either E.Error a)
 unwrap :: TestEnvironment a -> Either E.Error a
 unwrap (TestEnvironment x) = x
 
-mkTest :: String -> String -> Maybe Primitive-> Test
+mkTest :: String -> String -> Maybe Primitive -> Test
 mkTest name arg expected = TestCase . assertEqual name expected . eitherToMaybe . unwrap $ parser arg
   where
     eitherToMaybe (Right x) = Just x
     eitherToMaybe (Left _)  = Nothing
 
-assignment :: String -> String -> Primitive
-assignment = Assignment . either undefined id . varName
-
 testsParser :: Test
 testsParser = TestList [
-  mkTest "Empty spaces"                     "  \t  \t "                    $ Just $ Command [],
-  mkTest "Single command"                   "cmd"                          $ Just $ Command ["cmd"],
-  mkTest "Single command with spaces"       "   cmd   "                    $ Just $ Command ["cmd"],
-  mkTest "Other single command"             "qwer"                         $ Just $ Command $ "qwer" : [],
-  mkTest "Other single command with spaces" "   qwer   "                   $ Just $ Command $ "qwer" : [],
-  mkTest "Empty assignment"                 "x="                           $ Just $ assignment "x" "",
-  mkTest "Empty assignment with spaces"     "   x=   "                     $ Just $ assignment "x" "",
-  mkTest "Assignment"                       "x=1234"                       $ Just $ assignment "x" "1234",
-  mkTest "Assignment with spaces"           "   x=1234   "                 $ Just $ assignment "x" "1234",
-  mkTest "Assignment failure"               "x=1234 cmd"                   $ Nothing,
-  mkTest "Command with arguments"           "cmd arg1 arg2 arg3"           $ Just $ Command $ "cmd" : ["arg1", "arg2", "arg3"],
-  mkTest "Single quotes"                    "'x='"                         $ Just $ Command ["x="],
-  mkTest "Double quotes"                    "\"x=\""                       $ Just $ Command ["x="],
-  mkTest "Backslash ="                      "x\\="                         $ Just $ Command ["x="],
-  mkTest "Quoted command"                   "c'm 'd a\"rg1' ar\"g2' arg3'" $ Just $ Command $ "cm d" : ["arg1' arg2 arg3"],
-  mkTest "Quotes and spaces"                " cmd 'arg 1' \"arg 2\" "      $ Just $ Command $ "cmd" : ["arg 1", "arg 2"],
-  mkTest "Terminated single quote"          "cmd 'arg1"                    $ Nothing,
-  mkTest "Terminated double quote"          "cmd \"arg1 \\\""              $ Nothing,
-  mkTest "Backslashes"                      "cmd arg\\ 1 arg\\ 2"          $ Just $ Command $ "cmd" : ["arg 1", "arg 2"],
-  mkTest "Backslash space"                  "    \\    "                   $ Just $ Command $ " " : [],
-  mkTest "Escaped quotes"                   "cmd \\\"arg1\\\"\\ arg2"      $ Just $ Command $ "cmd" : ["\"arg1\" arg2"],
-  mkTest "Empty"                            ""                             $ Just $ Command []
+  mkTest "Empty spaces"                     "  \t  \t "                             $ Just $ Commands [],
+  mkTest "Single command"                   "cmd"                                   $ Just $ Commands [["cmd"]],
+  mkTest "Single command with spaces"       "   cmd   "                             $ Just $ Commands [["cmd"]],
+  mkTest "Other single command"             "qwer"                                  $ Just $ Commands [["qwer"]],
+  mkTest "Other single command with spaces" "   qwer   "                            $ Just $ Commands [["qwer"]],
+  mkTest "Empty assignment"                 "x="                                    $ Just $ Assignment "x" "",
+  mkTest "Empty assignment with spaces"     "   x=   "                              $ Just $ Assignment "x" "",
+  mkTest "Assignment"                       "x=1234"                                $ Just $ Assignment "x" "1234",
+  mkTest "Assignment with spaces"           "   x=1234   "                          $ Just $ Assignment "x" "1234",
+  mkTest "Assignment failure"               "x=1234 cmd"                            $ Nothing,
+  mkTest "Command with arguments"           "cmd arg1 arg2 arg3"                    $ Just $ Commands [["cmd", "arg1", "arg2", "arg3"]],
+  mkTest "Single quotes"                    "'x='"                                  $ Just $ Commands [["x="]],
+  mkTest "Double quotes"                    "\"x=\""                                $ Just $ Commands [["x="]],
+  mkTest "Backslash ="                      "x\\="                                  $ Just $ Commands [["x="]],
+  mkTest "Quoted command"                   "c'm 'd a\"rg1' ar\"g2' arg3'"          $ Just $ Commands [["cm d", "arg1' arg2 arg3"]],
+  mkTest "Quotes and spaces"                " cmd 'arg 1' \"arg 2\" "               $ Just $ Commands [["cmd", "arg 1", "arg 2"]],
+  mkTest "Terminated single quote"          "cmd 'arg1"                             $ Nothing,
+  mkTest "Terminated double quote"          "cmd \"arg1 \\\""                       $ Nothing,
+  mkTest "Backslashes"                      "cmd arg\\ 1 arg\\ 2"                   $ Just $ Commands [["cmd", "arg 1", "arg 2"]],
+  mkTest "Backslash space"                  "    \\    "                            $ Just $ Commands [[" "]],
+  mkTest "Escaped quotes"                   "cmd \\\"arg1\\\"\\ arg2"               $ Just $ Commands [["cmd", "\"arg1\" arg2"]],
+  mkTest "Basic pipes"                      "cmd1|cmd2|cmd3"                        $ Just $ Commands [["cmd1"], ["cmd2"], ["cmd3"]],
+  mkTest "Pipes with args"                  "cmd1 arg1|cmd2 arg2|cmd3 arg3"         $ Just $ Commands [["cmd1", "arg1"], ["cmd2", "arg2"], ["cmd3", "arg3"]],
+  mkTest "Pipes with spaces"                "  cmd1 | cmd2 | cmd3  "                $ Just $ Commands [["cmd1"], ["cmd2"], ["cmd3"]],
+  mkTest "Pipes with args and spaces"       "  cmd1 arg1 | cmd2 arg2 | cmd3 arg3  " $ Just $ Commands [["cmd1", "arg1"], ["cmd2", "arg2"], ["cmd3", "arg3"]],
+  mkTest "Empty"                            ""                                      $ Just $ Commands []
   ]
