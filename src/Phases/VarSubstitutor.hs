38874dd71f38bd4e1f15d527b4bc62f1186a1542
Anton Surkis <antonsurkis@gmail.com>
2023-03-09 23:55:40 +0300

    Cover the "'$x'" case

--- src/Phases/VarSubstitutor.hs
+++ src/Phases/VarSubstitutor.hs
@@ -3,7 +3,7 @@ module Phases.VarSubstitutor (varSubstitutor) where
 import           Data.Variable
 import           Environment.MonadError     (Error (..), MonadError, throwError)
 import           Environment.MonadVarReader
-import           Phases.Parser
+import           Phases.Parser              (singleQuotes)
 
 -- | Сконструировать специфичную для модуля ошибку
 modError :: String -> Error
@@ -19,23 +19,30 @@ throwModError = throwError . modError
 -- Учитываются кавычки, разбор строки не производится.
 varSubstitutor :: (MonadError m, MonadVarReader m) => String -> m String
 varSubstitutor "" = pure ""
-varSubstitutor ('\\' : '\'' : cs) = ("\\\'" ++) <$> varSubstitutor cs
-varSubstitutor ('\'' : cs) = do
-  (s, r) <- singleQuotes cs
-  r' <- varSubstitutor r
-  pure $ "'" ++ s ++ "'" ++ r'
-varSubstitutor ('$' : '{' : cs) = case readVariable cs of
-  Just (var, '}' : r) -> getVar var >>= \val -> (escapeStr val ++) <$> varSubstitutor r
-  Just _ -> throwModError "Expected }"
-  Nothing -> throwModError "Empty variable name"
-varSubstitutor ('$' : cs) = case readVariable cs of
-  Just (var, r) -> getVar var >>= \val -> (escapeStr val ++) <$> varSubstitutor r
-  Nothing -> throwModError "Empty variable name"
-
--- Отдельно обрабатывать двойные кавычки не нужно,
--- они не влияют на подстановку переменных.
--- Семантически они обрабатываются на следующей стадии.
+varSubstitutor ('\\' : c : cs)  = ('\\' :) . (c :) <$> varSubstitutor cs
+varSubstitutor ('\'' : cs) = singleQuotes cs >>= \(s, r) -> (++) ("'" ++ s ++ "'") <$> varSubstitutor r
+varSubstitutor ('$' : cs) = parseVariable varSubstitutor cs
+varSubstitutor ('\"' : cs) = ('\"' :) <$> doubleQuotes cs
 varSubstitutor (c : cs) = (c :) <$> varSubstitutor cs
+
+-- | Обработка внутри двойных кавычек.
+-- Отличается тем, что одинарные не обрабатываются отдельно.
+doubleQuotes :: (MonadError m, MonadVarReader m) => String -> m String
+doubleQuotes "" = throwModError "Unexpected end of line in double quotes"
+doubleQuotes ('\\' : c : cs)  = ('\\' :) . (c :) <$> doubleQuotes cs
+doubleQuotes ('$' : cs) = parseVariable doubleQuotes cs
+doubleQuotes ('\"' : cs) = ('\"' :) <$> varSubstitutor cs
+doubleQuotes (c : cs) = (c :) <$> doubleQuotes cs
+
+parseVariable :: (MonadError m, MonadVarReader m) => (String -> m String) -> String -> m String
+parseVariable _ "" = throwModError "Empty variable name"
+parseVariable retTo ('{' : cs) = case readVariable cs of
+  Just (var, '}' : r) -> getVar var >>= \val -> (escapeStr val ++) <$> retTo r
+  Just _              -> throwModError "Expected }"
+  Nothing             -> throwModError "Empty variable name"
+parseVariable retTo cs = case readVariable cs of
+  Just (var, r) -> getVar var >>= \val -> (escapeStr val ++) <$> retTo r
+  Nothing       -> throwModError "Empty variable name"
 
 -- | Экранирование строки.
 escapeStr :: String -> String
